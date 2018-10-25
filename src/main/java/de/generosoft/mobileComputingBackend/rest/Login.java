0e99f344b69e74dbcdcbce6bcb86f744ab7a2eda
yannnickweber <yannick.weber95@googlemail.com>
2018-10-25 18:03:48 +0200

    made field final @NotNull

--- src/main/java/de/generosoft/mobileComputingBackend/rest/Login.java
+++ src/main/java/de/generosoft/mobileComputingBackend/rest/Login.java
@@ -1,14 +1,13 @@
 package de.generosoft.mobileComputingBackend.rest;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Login {
 
-    private String role;
+    private final @NotNull String role;
 
-    public Login(String role) {
+    public Login(final @NotNull String role) {
         this.role = role;
     }
 
-    public String getRole() {
-        return role;
-    }
 }
